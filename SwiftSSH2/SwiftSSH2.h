9f5d481f78ea243eeb1985203852e53bd7c6cd7f
esttorhe <me@estebantorr.es>
2015-04-30 17:09:32 -0600

    Initial Commit

---
+++ SwiftSSH2/SwiftSSH2.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSSH2.h
+//  SwiftSSH2
+//
+//  Created by Esteban Torres on 30/4/15.
+//  Copyright (c) 2015 Esteban Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSSH2.
+FOUNDATION_EXPORT double SwiftSSH2VersionNumber;
+
+//! Project version string for SwiftSSH2.
+FOUNDATION_EXPORT const unsigned char SwiftSSH2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSSH2/PublicHeader.h>
+
+
