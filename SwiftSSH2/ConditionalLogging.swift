6c8d8d1c4c28832d5d0f000461f33ef06131620e
Esteban Torres <me@estebantorr.es>
2015-09-16 15:01:47 -0600

    Minor Swift 2.0 GM fixes

--- SwiftSSH2/ConditionalLogging.swift
+++ SwiftSSH2/ConditionalLogging.swift
@@ -5,7 +5,7 @@
 */
 func println(object: Any) {
   #if DEBUG
-    Swift.print(object, appendNewline: true)
+    Swift.print(object, terminator: "\n")
   #endif
 }
 
@@ -15,6 +15,6 @@ when `-D DEBUG` is set on `Other Swift Flags`.
 */
 func print(object: Any) {
   #if DEBUG
-    Swift.print(object, appendNewline: false)
+    Swift.print(object, terminator: "")
   #endif
 }
